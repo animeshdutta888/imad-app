9a56ee4f1ef734fd875c702bb6503427d4148b69
Animesh Dutta <dutta.animesh08@gmail.com>
2018-02-24 12:34:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
 
 var app = express();
 app.use(morgan('combined'));
+var config={
+    user:'duttaanimesh08', 
+    database:'duttaanimesh08',
+    host:'db.imad.hasura-app.io',
+    port:'5432'
+    password: process.env.DB_PASSWORD
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -15,6 +24,17 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+var pool= new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.status(JSON.stringify(result));
+        }
+    });
 });
 app.get('/article-one',function(req,res){
   res.sendFile(path.join(__dirname, 'ui','article-one.html'));
