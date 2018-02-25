2914d78b7a4f708d2d4738d58d8074a7f0d9988d
Animesh Dutta <dutta.animesh08@gmail.com>
2018-02-25 17:43:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config={
     database:'duttaanimesh08',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:'config.db.password' };
+    password:'db-duttaanimesh08-68770' };
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
