498ff82db88a89e6a061003d09e6bb10959824f5
Animesh Dutta <dutta.animesh08@gmail.com>
2018-03-12 03:11:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,32 @@
-console.log('Loaded!');
+var submit=document.getElementById('submit_btn');
+submit.onClick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                console.log('Logged in');
+                alert("logged in successfully");
+            }
+            else if(request.status===403){
+                alert("Username/password is invalid");
+            }
+            else if(request.status===500){
+                alert("Something went wrong on server");
+            }
+        }
+    };
+ 
+ 
+ 
+ 
+ 
+     var username=document.getElementById('username').value;
+     var password=document.getElementById('password').value;
+     console.log('username');
+     console.log('password');
+     request.open('POST','http://duttaanimesh08.imad.hasura-app.io/',true)
+     request.setRequestHeader('Content-Type',application/json);
+     request.send(JSON.stringify({username:username,password:password}));
+    
+};
