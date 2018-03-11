7328904c2e174babc7dbe8ea7d279ae7536d5f31
Animesh Dutta <dutta.animesh08@gmail.com>
2018-03-12 01:40:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
-var crypto=require('crypto');
-
 var app = express();
+var crypto=require('crypto');
+var bodyParser=require('body-parser');
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 var config={
     user:'duttaanimesh08', 
     database:'duttaanimesh08',
@@ -81,6 +82,20 @@ function hash(input,salt){
 app.get('/hash/:input',function(req,res){
    var hashedString=hash(req.params.input,'this-is-some-random-string');
    res.send(hashedString);
+});
+app.post('/create-user',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.getRandomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.status(200).send("User created"+username);
+        }
+    });
 });
 
 var pool= new Pool(config);
