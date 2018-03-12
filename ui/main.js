be4067ee747ceb99e4fed929387f3765c2ef61c4
Animesh Dutta <dutta.animesh08@gmail.com>
2018-03-12 11:21:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,7 @@ submit.onClick=function(){
      var password=document.getElementById('password').value;
      console.log('username');
      console.log('password');
-     request.open('POST','http://duttaanimesh08.imad.hasura-app.io/',true);
+     request.open('POST','/login',true);
      request.setRequestHeader('Content-Type','application/json');
      request.send(JSON.stringify({username:username,password:password}));
     
