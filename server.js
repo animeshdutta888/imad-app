4a13ecd5d1042eed322d1b00b91d5dd7885618f2
Animesh Dutta <dutta.animesh08@gmail.com>
2018-03-18 01:11:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.get('/check-login',function(req,res){
          res.send('You are not logged in')
      }
 });
-app.get('logout',function(req,res){
+app.get('/logout',function(req,res){
  delete req.session.auth;
  res.send('Logged out');
 });
