0a2fe13222087ded6c41e6df6e54df7530170407
Animesh Dutta <dutta.animesh08@gmail.com>
2018-03-29 01:39:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ app.get('/check-login',function(req,res){
      }
      else{
      
-         res.send('You are not logged in')
+         res.send('You are not logged in');
      }
 });
 app.get('/logout',function(req,res){
