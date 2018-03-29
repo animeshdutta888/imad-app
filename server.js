2537b12fc938d5ee3612404a622d12026d947432
Animesh Dutta <dutta.animesh08@gmail.com>
2018-03-29 17:14:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,8 +121,8 @@ app.post('/login',function(req,res){
             var hashedPassword=hash(password,salt);
             if(hashedPassword===dbString)
                 {
-                req.session.auth={userId: result.rows[0].id};
-                res.send('Credentials correct');
+                req.session.auth={ userId:result.rows[0].id};
+                res.status(200).send('Credentials correct');
             }
             else
             {
